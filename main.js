2f23fb41d7d05ad77fdaedbd5f26c1cf2a63fb40
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-05-04 12:23:12 -0300

    style

--- main.js
+++ main.js
@@ -47,4 +47,20 @@ minutos +
 segundos +
 " segundos"
 );
+} else {
+return "Prazo Finalizado";
 }
+}
+
+function atualizaCronometro() {
+for (let i = 0; i < contadores.length; i++) {
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+
+function comecaCronometro() {
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
