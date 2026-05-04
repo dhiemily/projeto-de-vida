7566ff1f9d4c3865d915152f5f9d639d50d04cff
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-05-04 12:22:14 -0300

    s

--- main.js
+++ main.js
@@ -22,20 +22,21 @@ const tempoObjetivo4 = new Date("2026-06-01T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 
-let tempoAtual = new Date();
+
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
-  segundos %= 60;
-minutos %= 60;
-horas %= 24;
-let tempoAtual = new Date();
+  let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo - tempoAtual;
 let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
+  segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
 return (
 dias +
 " dias " +
