cc028e54fafcc6eaad24f8a989c00385a4e97364
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-05-14 09:58:53 -0300

    s

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
   document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
   document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 for (let i = 0; i < contadores.length; i++) {
-//contadores[i].textContent = calculaTempo(tempos[i]);
+contadores[i].textContent = calculaTempo(tempos[i]);
  }
 }
 
