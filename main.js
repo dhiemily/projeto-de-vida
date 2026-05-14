c547c7afbfd26faf88467a94d2abfba3978518c4
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-05-14 10:00:42 -0300

    s

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
-
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
@@ -41,10 +40,10 @@ return [0, 0, 0, 0];
 }
 
 function atualizaCronometro() {
-document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-  document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-  document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+  document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+  document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
 for (let i = 0; i < contadores.length; i++) {
 contadores[i].textContent = calculaTempo(tempos[i]);
  }
