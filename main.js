519ac895cace395647b87b082f6586c58d7740be
dhiemily <167090493+dhiemily@users.noreply.github.com>
2026-05-14 10:02:21 -0300

    s

--- main.js
+++ main.js
@@ -23,20 +23,21 @@ const tempoObjetivo4 = new Date("2026-06-01T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
-let tempoAtual = new Date();
-let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24);
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-if (tempoFinal > 0) {
-return [dias, horas, minutos, segundos];
-} else {
-return [0, 0, 0, 0];
-}
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  if (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0, 0, 0, 0];
+  }
 }
 
 function atualizaCronometro() {
